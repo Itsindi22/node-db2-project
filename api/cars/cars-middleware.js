e72d051259534b56694f042df59f3f1d7d6e6306
Inderpreet <inderpreet2004123@gmail.com>
2025-10-20 11:40:21 -0400

    8passing

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -36,6 +36,7 @@ const checkCarPayload = (req, res, next) => {
 }
 
 const checkVinNumberValid = (req, res, next) => {
+  const {vin} =req.body
   if (vin.validate(req.params.vin)) {
 next()
   } else {
